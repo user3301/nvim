463a7235d8f7fbb2e748c5234990800f5e4b7d2e
user3301 <zhelun.gai@gmail.com>
2024-06-10 21:02:16 +1000

    make neotree show dotfiles

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,19 +7,15 @@ return {
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_by_name = {
-          -- ".DS_Store",
-          -- "thumbs.db",
-        },
+  config = function()
+    require("neo-tree").setup{
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+        }
       }
     }
-  },
-  config = function()
     vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {})
   end
 }
