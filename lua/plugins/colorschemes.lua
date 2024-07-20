de843137d31a1a86ba38753117d38b5a68de9d14
user3301 <zhelun.gai@anz.com>
2024-07-20 13:29:52 +1000

    add keymap for changing color scheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,6 +1,15 @@
 return {
-'morhetz/gruvbox',
-config = function()
-        vim.cmd.colorscheme("gruvbox")
-end
+  {
+    'morhetz/gruvbox',
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end
+  },
+  vim.keymap.set("n", "<leader>sp", ":lua require('telescope.builtin').colorscheme({enable_preview = true})<CR>",
+    { noremap = true, silent = true })
 }
